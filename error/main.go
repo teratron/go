0b638f917fcea53c1210b7f3ce066c9ab2cf6748
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-01-04 19:25:01 +0200

    Learning web.

--- error/main.go
+++ error/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 type errorOne struct {
@@ -21,4 +22,57 @@ func main() {
 	fmt.Println(e1, " / ", errors.Unwrap(e1))
 	fmt.Println(e2, " / ", errors.Unwrap(e2))
 	fmt.Println(e3, " / ", errors.Unwrap(e3))
+	fmt.Println()
+
+	// func Is(err, target error) bool
+	var err1 errorOne
+	var err2 error = errorOne{}
+	var err3 = fmt.Errorf("e3: %w", errorOne{})
+
+	fmt.Printf("%v(%T) - %v(%T)\n", err1, err1, err2, err2)
+	if err1 == err2 {
+		fmt.Println("Equality Operator: Both errors are equal")
+	}
+	if errors.Is(err1, err2) {
+		fmt.Println("Is function: Both errors are equal")
+	}
+
+	fmt.Printf("\n%v(%T) - %v(%T)\n", err1, err1, err3, err3)
+	if err1 == err3 {
+		fmt.Println("Equality Operator: Both errors are equal")
+	} else {
+		fmt.Println("Equality Operator: Both errors are not equal")
+	}
+	if errors.Is(err3, err1) {
+		fmt.Println("Is function: Both errors are equal")
+	}
+	fmt.Println()
+
+	// func As(err error, target interface{}) bool
+	err := openFile("non-existing.txt")
+
+	if e, ok := err.(*os.PathError); ok {
+		//fmt.Printf("Using Assert: Error e is of type path error. Path: %v\n", e.Path)
+		fmt.Printf("Using Assert: Error e is of type path error. Error: %v\n", e)
+	} else {
+		fmt.Println("Using Assert: Error not of type path error")
+	}
+	fmt.Println()
+
+	var pathError *os.PathError
+	fmt.Println(err, " / ", pathError)
+	if errors.As(err, &pathError) {
+		fmt.Println(err, " / ", pathError)
+		//fmt.Printf("Using As function: Error e is of type path error. Path: %v\n", pathError.Path)
+		fmt.Printf("Using As function: Error e is of type path error. Error: %v\n", pathError)
+	}
+}
+
+func openFile(fileName string) error {
+	_, err := os.Open(fileName)
+	if err != nil {
+		return err
+		//return fmt.Errorf("error opening: %w", err)
+	}
+	return nil
 }
--- website/main.go
+++ website/main.go
@@ -1,5 +1,75 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
+type User struct {
+	Name    string
+	Age     uint8
+	Money   float64
+	Hobbies []string
+}
+
+func (u *User) getInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: $%d", u.Name, u.Age, u.Money)
+}
+
+func (u *User) setName(name string) {
+	u.Name = name
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Name:    "Bob",
+		Age:     23,
+		Money:   2100,
+		Hobbies: []string{"Skate", "Dance", "Football"},
+	}
+	user.setName("Alex")
+	//_, err := fmt.Fprintf(w, bob.getInfo())
+	//_, err := fmt.Fprintf(w, "<b>%s</b>", "Bold text")
+	//_, err := fmt.Fprintf(w, `<b>%s</b>
+	//					<h1>Header</h1>`, "Bold text")
+
+	tmpl, err := template.ParseFiles("templates/home.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = tmpl.Execute(w, &user)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func contactsPage(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Contacts")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/contacts/", contactsPage)
+
+	err := http.ListenAndServe(":8083", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	/*bob := User{
+		name: "Bob",
+		age: 23,
+		money: 21000,
+		grades: .2,
+		happiness: 23,
+	}*/
+
+	handleRequest()
 }
