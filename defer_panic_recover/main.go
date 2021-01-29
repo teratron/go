fbf6c2275719f60323ef3443e5a091393e2723fa
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-01-29 17:36:42 +0200

    defer_panic_recover

---
+++ defer_panic_recover/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	f()
+}
+
+// Функция f откладывает функцию, которая вызывает recover и печатает восстановленное значение (если оно не равно нулю).
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+// Функция g принимает int i и паникует, если i больше 3, иначе она вызывает себя с аргументом i + 1.
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
